d9d2bceb992bc26c538460e3e46abcec033956b9
Sudarat1403 <sudaratraprit.com02@gmail.com>
2017-11-19 15:18:45 +0700

    Add infor

--- src/oop_lab/s359211110055/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110055/rmutsv/com/HelloWorld.java
@@ -6,6 +6,9 @@ public class HelloWorld {
 public static void main(String[] args) {
 
     System.out.println("Hello World, I'am FAR");
+    System.out.println("Sudarat Raprit");
+    System.out.println("Information system");
+    System.out.println("IS: RMUTSV");
 
 
 
