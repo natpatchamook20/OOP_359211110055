de4b406a95fe6bf13eb7d4a0dd6473d3e0ca8046
LAB_COM-MT_1 <sudaratraprit.com02@gmail.com>
2017-12-06 11:45:14 +0700

    TestMethods

--- src/ooplab4/OverLoadMethod.java
+++ src/ooplab4/OverLoadMethod.java
@@ -1,13 +1,25 @@
 package ooplab4;
+//Overlosding method
 
 public class OverLoadMethod {
-    public static void main (String [] args) {
-
-
-
-
-
-
+    public static int number = 100;
+    public static void Summation(int x,int y) {
+        System.out.println(x+y);
+        int t = number + x + y;
+    }
+    public static void Summation (){
+        System.out.println("Hello"+number);
+    }
+    public static void Summation(int x,int y,int z){
+        System.out.println(x+y+z+number);
+    }
+    public static void main(String[] args) {
+        Summation();
+        Summation(5,5);
+        Summation(3,5,7);
 
     }//main
+
+
+
 }//class
--- src/ooplab4/TestMethods.java
+++ src/ooplab4/TestMethods.java
@@ -1,38 +1,31 @@
 package ooplab4;
 
 public class TestMethods {
-    //Method types
-    //type 1
-    public static void A() {
-        System.out.println("Hello A");
-    }//A
+    //    method types
+ //    type 1
+         public static void A() {
+            System.out.println("Hello A");
+        }//A
+ //    type 2
+         public static void B(int x, int y) {
+            System.out.println(x+" "+y);
+        }//B
+ //    type 3
+         public static int C(int x, int y) {
+            return x + y;
+        }//C
 
-    //type 2
-    public static void B(int x, int y) {
-        System.out.println(x+" "+y);
-    }
-    //type 3
-    public static int C(int x, int y){
-        return x+y;
-    }
+            public static void main(String[] args) {
+            System.out.println("Hello");
+                A();
+                B(5, 10);
+               int x = 10, y = 20;
+                B(x, y);
+        //        type 3 return value management
+                //        1
+                                int num = C(x, y);
+                System.out.print(num);
 
-    public static void main (String [] args) {
-        System.out.println("Hello");
-        A();
-        B(x:5, y:10);
-        int x = 10, y = 20;
-        B(x, y);
-        //type 3 return value management
-        //1
-        int num = C(x, y);
-        System.out.println(num);
+                    } //main
 
-
-    }
-
-
-
-
-
-    }//main
-}//class
+         } //class
