8904b74dd36f7b956983ef18bf5ba2590f11fb67
sudarat1403 <sudaratraprit.com02@gmail.com>
2017-12-13 10:46:53 +0700

    Array2D

--- src/ooplab5/Array2D.java
+++ src/ooplab5/Array2D.java
@@ -1,13 +1,34 @@
 package ooplab5;
 
+import java.util.Scanner;
+
+import static ooplab4.OverLoadMethod.number;
+
 public class Array2D {
-    public static void main(String[] args) {
+    public  static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int number [][]=new int [2][3];
+        for (int i=0;i<number.length;i++){
+            for (int j=0;j<number[i].length;j++){
 
+                System.out.print("Enter an integer: ");
+                number[i][j] = scanner.nextInt();
+            }//for j
+        }//for i
 
-
-
-
-
-
+        showArray(number);
     }//main
+    private static void showArray(int[][]number){
+        System.out.println("Data in Array: ");
+        for (int i=0;i<number.length;i++){
+            for (int j=0;j<number[i].length;j++){
+                System.out.print(number[i][j]+" ");
+            }//for j
+        }//for i
+
+
+
+    }//showArray
+
+
 }//class
