70f5177834bd82808d37b061b7556b57e4241fd2
sudarat1403 <sudaratraprit.com02@gmail.com>
2017-12-13 11:45:29 +0700

    sortingArray1

--- src/ooplab5/sortingArray.java
+++ src/ooplab5/sortingArray.java
@@ -1,5 +1,8 @@
 package ooplab5;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class sortingArray {
@@ -8,15 +11,30 @@ public class sortingArray {
     private static int number[]=new int [MAX];
     public static void main(String[] args) {
         inputData(number,number.length);
-        System.out.println("Before sorting: ");
+
         showData(number,number.length);
         findMaxData(number);
-        // findMinData(number);
-        // SortingData(number);
-    }
+        findMinData(number);
+        System.out.println("Before sorting: ");
+        //ascending order
+        SortingDataAscending(number);
 
-    private static void SortingData(int[] number) {
-    }
+        // sortingDataDesending(number);
+
+    }//main
+
+    private static void sortingDataDesending(int[] number) {
+        System.out.println("Desending roder: ");
+        Integer[] num = {1,8,6,3};
+        Arrays.sort(num, Collections.reverseOrder());
+        showData(number,number.length);
+    }//sortingDataDesending
+
+    private static void SortingDataAscending (int[] number) {
+        System.out.println("Ascending roder: ");
+        Arrays.sort(number);
+        showData(number,number.length);
+    }//SortingDataAscending
 
     private static void findMinData(int[] number) {
     }
@@ -27,7 +45,7 @@ public class sortingArray {
             if (min>=number[i])
                 min = number[i];
         }//for
-        System.out.println("The maximum data: " + min);
+        System.out.println("The minimum data: " + min);
     }//findMaxData
 
     private static void showData(int[] number, int length) {
